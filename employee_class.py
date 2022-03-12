bf4c5618826453192e4ea225abda09cd9191538f
1abz <100859634+1abz@users.noreply.github.com>
2022-03-12 10:15:47 +0000

    class database

---
+++ employee_class.py
@@ -0,0 +1,34 @@
+
+    class Employee:
+        def __init__(self, id, firstname, lastname, position, birth_day, birth_month, birth_year, graduated):
+            self.eid = id
+            self.efirst_name = firstname
+            self.elast_name = lastname
+            self.eposition = position
+            self.ebirth_day = birth_day
+            self.ebirth_month = birth_month
+            self.ebirth_year = birth_year
+            self.egraduated = graduated
+
+
+    def print_employee_info(self)
+        print(f'Employee ID: {self.eid}')
+        print(f"Employee first name: {self.efirst_name}")
+        print(f"('Employee last name: {self.elast_name}")
+        print(f"('Employee position: {self.eposition}")
+        print(f"(Employee birth day: {self.ebirth_day}")
+        print(f"(Employee birth month: {self.ebirth_month}")
+        print(f"(Employee birth year: {self.ebirth_year}")
+        print(f"(Employee graduated?  {self.egraduated}")
+
+
+#id, firstname, lastname, position, birth_day, birth_month, birth_year, graduated
+
+if __name__ == "__main__":
+
+    emp1 : Employee(1,abz,mo,manager,1,2,2002,y)
+    emp1.print_employee_info
+
+
+
+
